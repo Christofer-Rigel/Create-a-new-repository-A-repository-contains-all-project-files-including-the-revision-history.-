0a0ef39c5a1c6db25c483a6b10c92cf9470e4ad7
Christofer-Rigel <bhadityagoswami@gmail.com>
2025-10-03 22:29:42 +0530

    Add files via upload

---
+++ actvity.sql
@@ -0,0 +1,13 @@
+CREATE TABLE KINghenrythesixth (
+    SNO TEXT PRIMARY KEY,
+    SNAME TEXT,
+    STATUS INTEGER,
+    CITY TEXT
+);
+INSERT INTO KINghenrythesixth (SNO , SNAME , STATUS , CITY ) VALUES
+    ('S1', 'Bartholomew Megasnort III', 999999999999, 'Volcanic Glitteropolis'),
+    ('S2', 'Princess Chainsaw McFury', 888888888, 'Subterranean Bubblegum Barracks'),
+    ('S3', 'Lord Quantum Waffleblast', 7777777777777, 'Sky Fortress of Eternal Screaming'),
+    ('S4', 'Captain Cringe Thunderhug', 123456789012345, 'Haunted Taco Dimension'),
+    ('S5', 'Duke Noodlepants the Unreadable', 42525424524250, 'Interdimensional Lizard Spa');
+SELECT * FROM KINghenrythesixth;
