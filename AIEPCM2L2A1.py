717281452a75458b3afc22fac23d767969a298f2
Eclipx-1ne <bhadityagoswami@gmail.com>
2026-05-01 22:18:20 +0530

    Add files via upload

---
+++ AIEPCM2L2A1.py
@@ -0,0 +1,19 @@
+import cv2
+import matplotlib.pyplot as plt 
+
+image = cv2.imread(r"C:\Users\Aditya\Downloads\AIEPCM2L2_Activities_Boilerplate_code_(2)-7a3e\AIEPCM2L2 Activities Boilerplate code\example.jpg")
+
+image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+plt.imshow(image_rgb)
+plt.title("RGB Image")
+plt.show()
+
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+plt.imshow(gray_image, cmap='gray')
+plt.title("Grayscale Image")
+plt.show()
+
+cropped_image = image[50:200, 50:200]
+plt.imshow(cv2.cvtColor(cropped_image, cv2.COLOR_BGR2RGB))
+plt.title("Cropped Image")
+plt.show()
